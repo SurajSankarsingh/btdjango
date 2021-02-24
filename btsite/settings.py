29a7945636fba1ed174eee0d95df4335263caaef
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-02-24 16:09:20 -0400

    added base html and wrapped templates, setup static files in static folder

--- btsite/settings.py
+++ btsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'btsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT= (BASE_DIR / 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    (BASE_DIR / 'btsite/static')
+]
