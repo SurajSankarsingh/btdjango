bb5f1b20f24286f932f0a76b22d81f5f58362515
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-02-25 01:33:11 -0400

    added realtors and listings and customized the admin page

--- btsite/settings.py
+++ btsite/settings.py
@@ -123,8 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT= BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'btsite/static'
 ]
+
+#Media Folder Settings
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
